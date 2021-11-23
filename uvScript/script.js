7524d149e2489a504c93163509ac3b7e9947a8b9
Manish Kumar <manish7479dlp@gmail.com>
2021-11-23 21:22:46 +0530

    minor change.

--- uvScript/script.js
+++ uvScript/script.js
@@ -7,9 +7,7 @@ const nav = document.getElementById("developer");
 nav.addEventListener("click", () =>{
    let input = prompt("OTP has been sent to you on your mobile number end with **21.please enter it below.");
    if(input === "10600120008"){
-        window.location.href = "html/developer.html"
-
-
+        window.location.href = "html/developer.html";
    } else {
        alert("Wrong OTP.");
    }
