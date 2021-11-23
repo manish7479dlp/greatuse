0d6f3157279a3296612b4e5944f6f0973c361ea9
Manish Kumar <manish7479dlp@gmail.com>
2021-11-23 21:07:45 +0530

    Password checker is added into developer hub

--- uvScript/script.js
+++ uvScript/script.js
@@ -2,3 +2,15 @@ function progress(about){
     alert(`Sorry ${about} Page is not publish yet..`);
     // alert("hii");
 }
+
+const nav = document.getElementById("developer");
+nav.addEventListener("click", () =>{
+   let input = prompt("OTP has been sent to you on your mobile number end with **21.please enter it below.");
+   if(input === "10600120008"){
+        window.location.href = "../html/developer.html"
+
+
+   } else {
+       alert("Wrong OTP.");
+   }
+})
