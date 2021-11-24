632fe41ef5e63a5e10608db09333d30dd52ec718
Manish Kumar <manish7479dlp@gmail.com>
2021-11-24 12:48:29 +0530

    Some console proble is fix from javaScript logic

--- uvScript/script.js
+++ uvScript/script.js
@@ -3,12 +3,30 @@ function progress(about){
     // alert("hii");
 }
 
-const nav = document.getElementById("developer");
-nav.addEventListener("click", () =>{
-   let input = prompt("OTP has been sent to you on your mobile number end with **21.please enter it below.");
+function fun() {
+    let input = prompt("OTP has been sent to you on your mobile number end with 620*****21.please enter it below.");
    if(input === "10600120008"){
         window.location.href = "html/developer.html";
+   } else if(input === "") {
+       alert("OTP Filed is Blanked");
    } else {
-       alert("Wrong OTP.");
+       alert("Invalid OTP.");
    }
-})
+    
+}
+
+// const nav = document.getElementById("developer");
+// nav.addEventListener("click", () =>{
+//    let input = prompt("OTP has been sent to you on your mobile number end with **21.please enter it below.");
+//    if(input === "10600120008"){
+//         window.location.href = "html/developer.html";
+//    } else if(input === "") {
+//        alert("OTP Filed is Blanked");
+//    } else {
+//        alert("Invalid OTP.");
+//    }
+   
+//  })  
+   
+
+
